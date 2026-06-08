f1ff9629c30a4c2f93515ecd21359f3d9feb2b69
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-06-08 16:31:23 -0300

    feat CRUD de follow (crear y eliminar)

--- src/controllers/follow.controllers.js
+++ src/controllers/follow.controllers.js
@@ -1,10 +1,46 @@
 const { Follows, User } = require('../models');
 
-const obtenerSeguidos = async (requestAnimationFrame, res) => {
+const obtenerSeguidos = async (req, res) => {
     const Seguidos = follows.findAll({
         include: {
-            model: user,
-            as: ''
+            model: User,
+            as: 'following_user_nickname'
         }
+
+
     })
+}
+
+const crearFollow = async (req, res) => {
+    try {
+
+        const seguido = await Follow.create({
+            following_user_nickname: req.body.following_user_nickname,
+            followed_user_nickname: req.body.followed_user_nicname
+        })
+
+        res.status(201).json(seguido)
+
+    } catch (error) {
+        res.status(500).json({ error: `Hubo un error al registrar el seguimiento: ${error.message}` })
+    }
+}
+
+const eliminarFollow = async (req, res) => {
+
+    try {
+        await Follow.destroy({
+            where: {
+                following_user_nickname: req.body.following_user_nickname,
+                followed_user_nickname: req.body.followed_user_nickname
+            }
+        })
+        res.status(201).json("seguimiento eliminado")
+    } catch (error) {
+        res.status(500).json({ error: `Hubo un error al eliminar el seguimiento: ${error.message}` })
+    }
+}
+
+module.exports = {
+    crearFollow, eliminarFollow
 }
