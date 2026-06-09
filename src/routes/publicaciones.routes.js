43728e9255a1b0b907c416881d789629f205e659
Leonardo Murano <leomellimurano@gmail.com>
2026-06-09 16:55:18 -0300

    Version final

--- src/routes/publicaciones.routes.js
+++ src/routes/publicaciones.routes.js
@@ -31,11 +31,9 @@ const router = Router()
 
 router.get('/', publicacionesController.obtenerPublicaciones)
 router.get('/:id', validarPublicacionId, publicacionesController.obtenerPublicacion)
-<<<<<<< HEAD
 router.post('/', validarPublicacion, validarUsuarioExistenteEnBody, publicacionesController.crearPublicacion)
-=======
 router.post('/', validarUsuarioExistenteEnBody, validarPublicacion, publicacionesController.crearPublicacion)
->>>>>>> main
+
 router.put('/:id', validarPublicacionId, validarPublicacion, publicacionesController.editarPublicacion)
 router.delete('/:id', validarPublicacionId, publicacionesController.eliminarPublicacion)
 
